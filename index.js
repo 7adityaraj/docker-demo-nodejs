c54b00a4e98e8406f4de107496cd83d94c4f483c
Aditya raj <7adityaraj@gmail.com>
2019-10-25 18:56:47 +0300

    seventh

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! >>>>> This is test sample from aditya raj. Fifth test');
-  res.send('Hello World! >>>>> This is test sample from aditya raj. sixth test');
+  res.send('Hello World! >>>>> This is test sample from aditya raj. Fifth test and seventh');
 });
 
 var server = app.listen(3000, function () {
